3e7a66f5aab3f77098fbd6cfbfd78e1e34942111
neyjuniorr <118519647+neyjuniorr@users.noreply.github.com>
2023-07-28 00:07:26 -0300

    First Commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import Navbar from './Components/Navbar';
+import Home from "./Components/Home";
+import MediaSocial from "./Components/MediaSocial"
+import About from "./Components/About"
+import Portfolio from "./Components/Portfolio"
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <MediaSocial />
+      <About />
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
